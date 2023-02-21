7d070757c7bb2ca53a1b10e20e42ddec040e5256
S_Best <bestbest116@gmail.com>
2023-02-21 18:32:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <section>
+	    <h1>My First React App on Vite</h1>
+    </section>
+  )
+}
+
+export default App
